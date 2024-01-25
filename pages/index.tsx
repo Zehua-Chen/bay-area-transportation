a6e2f8946a7ff0371629eebffd9349dca8c8c998
Zehua Chen <zehua-chen@outlook.com>
2024-01-24 22:54:55 -0800

    [chore] Setup Next project

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+const Index = () => (
+  <main>
+    <section>
+      <h1>Transportation in Bay Area</h1>
+    </section>
+    <section>
+      <h2>Biking</h2>
+      <ul>
+        <li>Bridges</li>
+        <li>Hills</li>
+      </ul>
+    </section>
+  </main>
+);
+
+export default Index;
