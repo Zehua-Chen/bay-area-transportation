4dc88c9e41efeff7e588a544ab0c5419b504723c
Zehua Chen <zehua-chen@outlook.com>
2024-02-01 00:51:12 -0800

    [feat] Add more content

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,16 @@ const Index = () => (
     <section>
       <h2>Biking</h2>
       <ul>
-        <li>Bridges</li>
-        <li>Hills</li>
+        <li>Payload</li>
+        <ul>
+          <li>Some bike frames do not support racks and baskets</li>
+          <li>Bike baskets may not be able to hold a lot of things</li>
+        </ul>
+        <li>Hills and bridges</li>
+        <ul>
+          <li>Going up the mountain can be stressful for the knee</li>
+          <li>Going down the slope can test the quality of the brakes</li>
+        </ul>
       </ul>
     </section>
   </main>
