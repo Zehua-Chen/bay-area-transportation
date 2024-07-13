4de34456a9ca604402880367bd8eab1f81e278e1
Zehua Chen <zehua-chen@outlook.com>
2024-07-13 12:25:06 -0700

    [feat] Add more notes on public transportation

--- pages/public-transportation.tsx
+++ pages/public-transportation.tsx
@@ -9,10 +9,6 @@ const PublicTransportation: FC = () => (
       <ul>
         <li>Buses</li>
         <li>Bart</li>
-        <ul>
-          <li>Noisy</li>
-          <li>Unsafe</li>
-        </ul>
         <li>Caltrain</li>
         <li>
           <a href="https://commute.org" target="_blank">
@@ -20,6 +16,33 @@ const PublicTransportation: FC = () => (
           </a>
         </li>
       </ul>
+
+      <section>
+        <h2>Bart</h2>
+        <ul>
+          <li>Pros</li>
+          <ul>
+            <li>Connect west and east bay</li>
+            <li>Trains are frequent, even on weekends</li>
+          </ul>
+          <li>Cons</li>
+          <ul>
+            <li>Noisy</li>
+            <li>Unsafe</li>
+          </ul>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Caltrain</h2>
+        <ul>
+          <li>Cons</li>
+          <ul>
+            <li>Slower</li>
+            <li>Less frequent</li>
+          </ul>
+        </ul>
+      </section>
     </section>
   </Page>
 );
